44f7e4a9845c6210581b9bb720f38ba1f7e6c585
flyc <yayayahahahaooii@gmail.com>
2022-03-24 03:11:21 +0800

    refactored complete, for work-around

--- clim.js
+++ clim.js
@@ -7,7 +7,7 @@ const { resolve } = require('dns')
 
 const { TaskSystem, download } = require('npm-flyc')
 const { end } = require('cheerio/lib/api/traversing')
-const defaultTaskSetting = () => ({ randomDelay: 0 })
+const defaultTaskSetting = (randomDelay = 0) => ({ randomDelay })
 
 const result = []
 const srcArray = []
@@ -76,51 +76,94 @@ async function start() {
   if (getUrlError) return // TODO error check
 
   const { url, endPage, id } = response
-  const [eachPageList, eachPageError] = await getEachPageImagesLink({ url, endPage, id })
+  const [allImageLinkList, eachPageError] = await getEachPageImagesLink({ url, endPage, id })
   if (eachPageError) return // TODO error check
 
-  console.log(eachPageList)
+  const [allImageInfoList, imageInfoError] = await getEachImageInfo(allImageLinkList)
+  if (imageInfoError) return // TODO error check
+
+  await startDownload(allImageInfoList)
+  console.log('完成囉!!!!')
+
+  // TODO 遞迴檢查 getUrlInfo 的其他項目?
 }
 
-// 會被遞迴執行?
-function getUrlInfo(urlIndex, setting) {
-  const { cookie, urlList } = setting
-  const currentUrl = urlList[urlIndex]
+async function startDownload(list) {
+  const taskList = _create_task(list)
+  const taskNumber = 1
+  const task_search = new TaskSystem(taskList, taskNumber, defaultTaskSetting(500))
 
-  if (urlIndex >= urlList.length) return void console.log('COMPLETE >w<//')
+  let allPagesImagesArray = (await task_search.doPromise()).filter(result => result.status === 1)
+  // TODO retry, 而且是必要的
+  if (allPagesImagesArray.length !== taskList.length) {
+    console.log('有失敗的單一圖片下載!!!!!!')
+    console.log('有失敗的單一圖片下載!!!!!!')
+    console.log('有失敗的單一圖片下載!!!!!!')
+    console.log('有失敗的單一圖片下載!!!!!!')
+  }
+  allPagesImagesArray = allPagesImagesArray.map(({ data }) => data)
 
-  // console.log('your cookie is: ' + jsonContent.cookie);
-  stepMessage('Get Url Info')
-  console.log(`current fetch url: ${currentUrl}`)
+  return [allPagesImagesArray, null]
 
-  // TODO check what is this ? startPage = 1
-  return new Promise(resolve =>
-    request(createRequestHeader(currentUrl), function (error, _, body) {
-      if (error) {
-        showError('getUrlInfo', 'get url basic info failed!')
-        return resolve([null, error])
+  function _create_task(list) {
+    return list.map(info => {
+      const { src, sort, name, type, url, id } = info
+      const { directory } = globalVariable.folderMap[id]
+      const filePath = `${directory}/${name}.${type}`
+
+      return function () {
+        return download(url, filePath, {
+          headers: { Cookie: globalVariable.cookie }
+        })
       }
-
-      const $ = cheerio.load(body)
-
-      const endPage = getEndPage($)
-      if (isNaN(endPage)) return void showError('endPage', 'endPage is not a number')
-
-      const title = $('title').text().trim().replace(/ /g, '_')
-      const directory = SAVE_DIRECTORY + '/' + title.replace(/\W/g, '_')
-      const id = getId(currentUrl)
-      globalVariable.folderMap[id] = { directory, endPage, id, title, url: currentUrl }
-
-      if (!fs.existsSync(directory)) fs.mkdirSync(directory)
-
-      console.log('get url info success')
-      console.log("gallery's title: " + title)
-      console.log(`total page: ${endPage}`)
-      console.log(`save in directory: ${directory}`)
-
-      return resolve([{ endPage, directory, id, title, url: currentUrl }, null])
     })
-  )
+  }
+}
+
+async function getEachImageInfo(allImageLinkList) {
+  const taskList = _create_task(allImageLinkList)
+
+  const taskNumber = 2
+  const task_search = new TaskSystem(taskList, taskNumber, defaultTaskSetting(500))
+
+  let allPagesImagesArray = (await task_search.doPromise()).filter(result => result.status === 1)
+  // TODO retry, 而且是必要的
+  if (allPagesImagesArray.length !== taskList.length) {
+    console.log('有失敗的單一圖片頁面!!!!!!')
+    console.log('有失敗的單一圖片頁面!!!!!!')
+    console.log('有失敗的單一圖片頁面!!!!!!')
+    console.log('有失敗的單一圖片頁面!!!!!!')
+  }
+  allPagesImagesArray = allPagesImagesArray.map(({ data }) => data)
+
+  return [allPagesImagesArray, null]
+
+  function _create_task(list) {
+    return list.map(info => {
+      const { url } = info
+
+      return function () {
+        return new Promise((resolve, reject) => {
+          request(createRequestHeader(url), function (error, response, body2) {
+            if (error) {
+              showError('getImgSrcByLink', 'api errur!')
+              return reject(erro)
+            }
+
+            $ = cheerio.load(body2)
+            const linkObj = { ...info }
+            const imageDom = $('#img')
+
+            const src = imageDom.attr('src')
+            linkObj.src = src
+            linkObj.type = src.match(/\.(\w+)$/)[1]
+
+            return resolve(linkObj)
+          })
+        })
+      }
+    })
+  }
 }
 
 async function getEachPageImagesLink({ endPage, url: rowUrl, id }) {
@@ -166,9 +209,10 @@ async function getEachPageImagesLink({ endPage, url: rowUrl, id }) {
             for (let i = 0; i < list.length; i++) {
               temp = $(list[i]).attr('href')
               linkArray.push({
+                id,
                 url: temp,
                 name: temp.split('/')[5],
-                number: 40 * page + i + 1
+                sort: 40 * page + i + 1
               })
             }
 
@@ -180,84 +224,45 @@ async function getEachPageImagesLink({ endPage, url: rowUrl, id }) {
   }
 }
 
-function singlePageLoaded(totalNumber) {
-  countloaded++
-  if (countloaded == totalNumber) {
-    countloaded = 0
-    console.reset()
-    console.log('100%')
+// 會被遞迴執行?
+function getUrlInfo(urlIndex, setting) {
+  const { cookie, urlList } = setting
+  const currentUrl = urlList[urlIndex]
 
-    linkArray.sort(function (a, b) {
-      return a.number - b.number
-    })
+  if (urlIndex >= urlList.length) return void console.log('COMPLETE >w<//')
 
-    taskIndex = taskIndex < linkArray.length ? taskIndex : linkArray.length - 1
-    for (var i = 0; i <= taskIndex; i++) {
-      getImgSrcByLink(linkArray[i])
-    }
-  } else {
-    console.reset()
-    console.log(((countloaded * 100) / totalNumber).toFixed(2) + '%')
-  }
-}
+  // console.log('your cookie is: ' + jsonContent.cookie);
+  stepMessage('Get Url Info')
+  console.log(`current fetch url: ${currentUrl}`)
 
-function getImgSrcByLink(linkObj, totalNumber) {
-  request(
-    {
-      url: linkObj.url,
-      headers: {
-        Cookie: cookie
-      },
-      jar: true
-    },
-    function (error, response, body2) {
-      console.log('current url: ' + linkObj.url)
-      if (!error) {
-        $ = cheerio.load(body2)
-        var imgList = $('#img')
-
-        linkObj.src = imgList.attr('src')
-        linkObj.type = imgList.attr('src').split('.')[imgList.attr('src').split('.').length - 1]
-        srcArray.push(linkObj)
-
-        taskIndex++
-        if (taskIndex >= linkArray.length) {
-          if (srcArray.length === linkArray.length) {
-            console.log('get src complete! start download')
-
-            fs.writeFile(currentDirectory + '.json', JSON.stringify(srcArray), function () {
-              console.log('write download src into result.json for testing')
-              srcArray = []
-              linkArray = []
-              taskIndex = originTaskIndex
-              urlIndex++
-              getUrlInfo(urlIndex)
-            })
-            downloadTrigger()
-            return
-          }
-          console.reset()
-          console.log(taskIndex, linkArray.length, srcArray.length)
-        } else {
-          console.reset()
-          console.log(taskIndex, linkArray.length, srcArray.length)
-          getImgSrcByLink(linkArray[taskIndex])
-        }
-      } else {
-        console.log('getImgSrcByLink error! retry.' + error)
-        getImgSrcByLink(linkObj, totalNumber)
+  // TODO check what is this ? startPage = 1
+  return new Promise(resolve =>
+    request(createRequestHeader(currentUrl), function (error, _, body) {
+      if (error) {
+        showError('getUrlInfo', 'get url basic info failed!')
+        return resolve([null, error])
       }
-    }
-  )
-}
 
-function downloadTrigger() {
-  countloaded = 0
-  taskIndex = originTaskIndex <= srcArray.length ? originTaskIndex : srcArray.length
-  for (var i = 0; i <= taskIndex; i++) {
-    if (!srcArray[i]) continue
-    download(srcArray[i].src, currentDirectory, srcArray[i].name + '.' + srcArray[i].type)
-  }
+      const $ = cheerio.load(body)
+
+      const endPage = getEndPage($)
+      if (isNaN(endPage)) return void showError('endPage', 'endPage is not a number')
+
+      const title = $('title').text().trim().replace(/ /g, '_')
+      const directory = SAVE_DIRECTORY + '/' + title.replace(/\W/g, '_')
+      const id = getId(currentUrl)
+      globalVariable.folderMap[id] = { directory, endPage, id, title, url: currentUrl }
+
+      if (!fs.existsSync(directory)) fs.mkdirSync(directory)
+
+      console.log('get url info success')
+      console.log("gallery's title: " + title)
+      console.log(`total page: ${endPage}`)
+      console.log(`save in directory: ${directory}`)
+
+      return resolve([{ endPage, directory, id, title, url: currentUrl }, null])
+    })
+  )
 }
 
 console.reset = function () {
