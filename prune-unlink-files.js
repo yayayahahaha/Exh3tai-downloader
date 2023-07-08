ec5fac4f9d0e60d97a538607f0dd77e8099e4421
flyc <yayayahahahaooii@gmail.com>
2023-07-09 01:53:58 +0800

    done

--- prune-unlink-files.js
+++ prune-unlink-files.js
@@ -2,27 +2,48 @@ import { RAW_IMAGES_DIRETORY, SAVE_DIRECTORY } from './utils.js'
 import fs from 'fs'
 import path from 'path'
 
-const rawImages = fs.readdirSync(RAW_IMAGES_DIRETORY).map((file) => path.resolve(path.join(RAW_IMAGES_DIRETORY, file)))
-const saveImages = fs
-  .readdirSync(SAVE_DIRECTORY)
-  .map((folder) => {
-    const saveFolderPath = path.resolve(path.join(SAVE_DIRECTORY, folder))
-    if (!fs.lstatSync(saveFolderPath).isDirectory()) return null
+start()
+function start() {
+  console.log("Let's prune extra raw files!")
 
-    return fs.readdirSync(saveFolderPath).map((fileName) => path.join(saveFolderPath, fileName))
-  })
-  .filter(Boolean)
-  .flat()
-  .map((filePath) => {
+  const rawImages = fs
+    .readdirSync(RAW_IMAGES_DIRETORY)
+    .map((file) => path.resolve(path.join(RAW_IMAGES_DIRETORY, file)))
+  const saveImages = fs
+    .readdirSync(SAVE_DIRECTORY)
+    .map((folder) => {
+      const saveFolderPath = path.resolve(path.join(SAVE_DIRECTORY, folder))
+      if (!fs.lstatSync(saveFolderPath).isDirectory()) return null
+
+      return fs.readdirSync(saveFolderPath).map((fileName) => path.join(saveFolderPath, fileName))
+    })
+    .filter(Boolean)
+    .flat()
+    .map((filePath) => {
+      const { name } = path.parse(filePath)
+      return name.split('-')[1]
+    })
+
+  const saveImagesMap = Object.fromEntries(saveImages.map((item) => [item, true]))
+  const deleteList = rawImages.filter((filePath) => {
     const { name } = path.parse(filePath)
-    return name.split('-')[1]
+    const hash = name.split('-')[0]
+    return !saveImagesMap[hash]
   })
 
-const saveImagesMap = Object.fromEntries(saveImages.map((item) => [item, true]))
-const deleteList = rawImages.filter((filePath) => {
-  const { name } = path.parse(filePath)
-  const hash = name.split('-')[0]
-  return !saveImagesMap[hash]
-})
+  console.log(`There are ${rawImages.length} raw Images!`)
+  console.log(`There are ${saveImages.length} save Images!`)
 
-console.log(deleteList)
+  if (deleteList.length === 0) {
+    console.log('All raw images at least linked to one save images!')
+    return
+  }
+
+  console.log(`${deleteList.length} images in raw images are not linked to any save images.`)
+
+  console.log('Pruning...')
+
+  deleteList.forEach((path) => fs.unlinkSync(path))
+
+  console.log('Done!')
+}
