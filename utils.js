d311149abe176f8adfd4ab228600693360d00f1b
flyc <yayayahahahaooii@gmail.com>
2023-07-21 03:05:38 +0800

    wip of savedImages part, tuning some regular codes

--- utils.js
+++ utils.js
@@ -6,6 +6,7 @@ export const SAVE_DIRECTORY = './saveImg'
 export const RAW_IMAGES_DIRETORY = 'raw-images'
 export const LOG_DIRECTORY = './log'
 export const PREPARE_SUFFIX = '-preparing' // TODO(flyc): 這個也要去替換一下各個地方
+export const MAC_STORE_FILE_NAME = '.DS_Store'
 
 export const UNCOMPLETED_URL_LIST_LOG_PREFIX = 'uncompleted-url-list'
 export const REUSED_LIST_LOG_PREFIX = 'reused-url-list'
@@ -34,6 +35,10 @@ export function readSettingJson() {
   }
 }
 
+// TODO(flyc): document
+const RAW_TYPE_VALUE = 'raw'
+const SAVED_TYPE_VALUE = 'saved'
+
 /**
  * @typedef {Object} ImageInfo
  * @property {string} name - name only, exclude extension
@@ -54,22 +59,7 @@ export function readAllRawImages() {
 
   return imageNames.map((fullName) => {
     const fullPath = path.resolve(path.join(RAW_IMAGES_DIRETORY, fullName))
-    const { dir, name } = path.parse(fullPath)
-    const folder = dir.split(path.sep).pop()
-    const ext = path.extname(fullPath).split('.').pop()
-
-    const [hash, ...others] = name.match(/\w+/g)
-    const url = ['', ...others].join('/')
-
-    return {
-      name,
-      ext,
-      fullName,
-      folder,
-      fullPath,
-      hash,
-      url,
-    }
+    return _getImageInfoByPath(fullPath, { type: RAW_TYPE_VALUE })
   })
 }
 
@@ -77,4 +67,67 @@ export function readAllRawImages() {
  * @function readAllSavedImages
  * @description TODO(flyc): document
  * */
-export function readAllSavedImages() {}
+export function readAllSavedImages() {
+  return fs
+    .readdirSync(SAVE_DIRECTORY)
+    .slice(0, 3 /*TODO(flyc):testing codes*/)
+    .filter((name) => fs.lstatSync(path.resolve(path.join(SAVE_DIRECTORY, name))).isDirectory())
+    .reduce(
+      (result, folder) => {
+        const fullFolderPath = path.resolve(path.join(SAVE_DIRECTORY, folder))
+
+        const images = fs.readdirSync(fullFolderPath).map((name) => {
+          const fullPath = path.join(fullFolderPath, name)
+          return _getImageInfoByPath(fullPath, { type: SAVED_TYPE_VALUE })
+        })
+
+        // console.log('images:', images)
+
+        result.sortByFolder.push({ folderName: folder, images, url: _getUrlFromFolder(folder) })
+
+        return result
+      },
+      { flatImages: [], sortByFolder: [] }
+    )
+}
+
+// TODO(flyc): document
+function _getUrlFromFolder(folderName) {
+  return ['', ...folderName.match(/\w+/g).slice(-3)].join('/')
+}
+
+// TODO(flyc): document
+function _getImageInfoByPath(fullPath, { type } = {}) {
+  if (type == null) {
+    console.error('Parameter `type` is required.')
+    return null
+  }
+
+  const { dir, name, base: fullName } = path.parse(fullPath)
+  const folder = dir.split(path.sep).pop()
+  const ext = path.extname(fullPath).split('.').pop()
+
+  let hashIndex = null
+  const nameMatchedKeys = name.match(/\w+/g)
+  if (type === RAW_TYPE_VALUE) {
+    hashIndex = 0
+  } else if (type === SAVED_TYPE_VALUE) {
+    hashIndex = 1
+  } else {
+    console.error('_getImageInfoByPath: wrong parameter `type`, please check again.')
+    return null
+  }
+
+  const hash = nameMatchedKeys[hashIndex]
+  const url = ['', ...nameMatchedKeys.slice(hashIndex + 1)].join('/')
+
+  return {
+    name,
+    ext,
+    fullName,
+    folder,
+    fullPath,
+    hash,
+    url,
+  }
+}
