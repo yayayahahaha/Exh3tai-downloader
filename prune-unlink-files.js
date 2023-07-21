1aadfda3e8fbe668ed35d1f4797bafe484d7daeb
flyc <yayayahahahaooii@gmail.com>
2023-07-22 01:41:29 +0800

    update prune-unlink-files

--- prune-unlink-files.js
+++ prune-unlink-files.js
@@ -1,35 +1,16 @@
-import { RAW_IMAGES_DIRETORY, SAVE_DIRECTORY } from './utils.js'
+import { readAllRawImages, readAllSavedImages } from './utils.js'
 import fs from 'fs'
-import path from 'path'
 
 start()
 function start() {
   console.log("Let's prune extra raw files!")
 
-  const rawImages = fs
-    .readdirSync(RAW_IMAGES_DIRETORY)
-    .map((file) => path.resolve(path.join(RAW_IMAGES_DIRETORY, file)))
-  const saveImages = fs
-    .readdirSync(SAVE_DIRECTORY)
-    .map((folder) => {
-      const saveFolderPath = path.resolve(path.join(SAVE_DIRECTORY, folder))
-      if (!fs.lstatSync(saveFolderPath).isDirectory()) return null
+  const rawImages = readAllRawImages()
 
-      return fs.readdirSync(saveFolderPath).map((fileName) => path.join(saveFolderPath, fileName))
-    })
-    .filter(Boolean)
-    .flat()
-    .map((filePath) => {
-      const { name } = path.parse(filePath)
-      return name.split('-')[1]
-    })
+  const saveImages = readAllSavedImages().flatImages
+  const saveImagesMap = Object.fromEntries(saveImages.map((info) => [info.hash, true]))
 
-  const saveImagesMap = Object.fromEntries(saveImages.map((item) => [item, true]))
-  const deleteList = rawImages.filter((filePath) => {
-    const { name } = path.parse(filePath)
-    const hash = name.split('-')[0]
-    return !saveImagesMap[hash]
-  })
+  const deleteList = rawImages.filter((rawInfo) => !saveImagesMap[rawInfo.hash])
 
   console.log(`There are ${rawImages.length} raw Images!`)
   console.log(`There are ${saveImages.length} save Images!`)
