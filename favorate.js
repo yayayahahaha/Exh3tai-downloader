2a9fd2a6d2f50219ea8c459a6d2ae293a0d7b72c
flyc <yayayahahahaooii@gmail.com>
2024-11-03 22:45:32 +0800

    add script to add url to favorites

---
+++ favorate.js
@@ -0,0 +1,69 @@
+import fetch from 'node-fetch'
+
+import { readSettingInfo, checkParam, stepMessage } from './utils.js'
+
+start()
+
+async function start() {
+  console.log("Let's Go!")
+
+  stepMessage('Load setting.json')
+  const jsonContent = readSettingInfo()
+  if (jsonContent == null) return
+  if (!checkParam(jsonContent)) return
+
+  const { cookie, url: urlList } = jsonContent
+
+  for (let i = 0; i < urlList.length; i++) {
+    const url = urlList[i]
+    await addToFavorite(url, cookie)
+    console.log(`url ${url} 添加成功`)
+    await new Promise((r) => setTimeout(r, 500))
+  }
+
+  console.log('成功')
+}
+
+async function addToFavorite(url, cookie) {
+  console.log(`正要添加到 favorite 的 url: ${url}`)
+
+  const { pathname } = new URL(url)
+  const [, , gid, t] = pathname.split('/')
+
+  const queryString = new URLSearchParams({
+    act: 'addfav',
+    gid,
+    t,
+  }).toString()
+  const headers = createFavorateHeader(url, cookie)
+
+  return fetch(`https://exhentai.org/gallerypopups.php?${queryString}`, {
+    headers,
+    body: 'favcat=0&favnote=&apply=Add+to+Favorites&update=1',
+    method: 'POST',
+  })
+}
+
+function createFavorateHeader(url, cookie) {
+  const { pathname } = new URL(url)
+  const [, , gid, t] = pathname.split('/')
+
+  const queryObject = {
+    act: 'addfav',
+    gid,
+    t,
+  }
+
+  return {
+    cookie,
+    accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'zh-TW,zh;q=0.9',
+    'cache-control': 'max-age=0',
+    'content-type': 'application/x-www-form-urlencoded',
+    priority: 'u=0, i',
+    'upgrade-insecure-requests': '1',
+    Referer: `https://exhentai.org/gallerypopups.php?${new URLSearchParams(queryObject).toString()}`,
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+  }
+}
